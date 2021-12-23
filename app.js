5bab0e5d1271ac6392696ca3bde034fcec4f1cc5
Nicole Uva <nicouva@gmail.com>
2021-12-22 23:13:39 -0500

    adding API

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const puzzleBoard = document.querySelector('#puzzle')
 const solveButton = document.querySelector('#solve-button')
 const squares = 81
+const submission = []
 
 for (let i = 0; i < squares; i++) {
   const inputElement = document.createElement('input')
@@ -9,3 +10,42 @@ for (let i = 0; i < squares; i++) {
   inputElement.setAttribute('max', '9')
   puzzleBoard.appendChild(inputElement)
 }
+
+const joinValues = () => {
+  const inputs = document.querySelectorAll('input')
+  inputs.forEach(input => {
+    if (input.value) {
+      submission.push(input.value)
+    } else {
+      submission.push('.')
+    }
+  })
+  console.log(submission)
+}
+
+
+const solve = () => {
+
+  var options = {
+    method: 'POST',
+    url: 'https://solve-sudoku.p.rapidapi.com/',
+    headers: {
+      'content-type': 'application/json',
+      'x-rapidapi-host': 'solve-sudoku.p.rapidapi.com',
+      'x-rapidapi-key': '913867a828msh1d74930264ab4dep1c6cb5jsnbb6dc8151657'
+    },
+    data: {
+      puzzle: '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
+    }
+  };
+
+  axios.request(options).then(function (response) {
+    console.log(response.data);
+  }).catch(function (error) {
+    console.error(error);
+  });
+}
+
+
+
+solveButton.addEventListener('click', solve)
